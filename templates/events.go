91f2c171ec12bdebf712e7b873a09a1a7f457a12
Luis Flores <me@luisflores.mx>
2017-04-13 21:28:20 +0000

    Update templates

--- templates/events.go
+++ templates/events.go
@@ -46,17 +46,11 @@ func getFirstTemplate(world models.World) models.Event {
 }
 
 func getSecondTemplate(world models.World) models.Event {
-	time := time.Now().Unix()
-	rand.Seed(time)
-
-	possibleAgents := []int{2, 4}
-	agentID := rand.Intn(len(possibleAgents))
-
 	event := models.Event{
 		Name:        "The orcs are gathering",
 		Description: "This unusal situation means they are looking for something valuable",
 		Protagonist: false,
-		Agents:      []models.Agent{world.Agents[possibleAgents[agentID]]},
+		Agents:      []models.Agent{world.Agents[4]}, // Traveler
 		Location:    world.Locations[3],
 		Goal:        world.Goals[1], // Find the treasure
 		Propagation: 1,
@@ -71,9 +65,9 @@ func getThirdTemplate(world models.World) models.Event {
 		Name:        "The king has gathered important information",
 		Description: "This information can solve an imporantant mistery",
 		Protagonist: false,
-		Agents:      []models.Agent{world.Agents[0], world.Agents[1]}, // The king
-		Location:    world.Locations[2],
-		Goal:        world.Goals[0], // Talk to the king
+		Agents:      []models.Agent{world.Agents[0], world.Agents[1]}, // The king & Witness
+		Location:    world.Locations[1],                               // King
+		Goal:        world.Goals[0],                                   // Talk to the king
 		Propagation: 1,
 		Cause:       world.Goals[4], // Rescue the princess
 	}
@@ -86,7 +80,7 @@ func getFourthTemplate(world models.World) models.Event {
 		Name:        "Kill the dragon",
 		Description: "The most feared creature in this world, this dragon has a broad, elongated body with a very long tail",
 		Protagonist: true,
-		Agents:      []models.Agent{world.Agents[1]},
+		Agents:      []models.Agent{world.Agents[3]}, // Wizard
 		Location:    world.Locations[4],
 		Goal:        world.Goals[3], // Fight the dragon
 		Propagation: 1,
@@ -105,7 +99,7 @@ func getFifthTemplate(world models.World) models.Event {
 	event := models.Event{
 		Name:        "The dragon has taken the princess",
 		Description: "The princess disappeared at midnight, some people saw the dragon close to the city last night",
-		Agents:      []models.Agent{world.Agents[2]}, // Wizard
+		Agents:      []models.Agent{world.Agents[4]}, // Princess
 		Location:    world.Locations[possibleCities[cityID]],
 		Goal:        world.Goals[4], // Rescue the princess
 		Propagation: 2,
